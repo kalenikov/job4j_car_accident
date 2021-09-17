9a5d6d4d5014f0a6de3f79b9af5d8038239d0930
serj <sergeykalenikov@gmail.com>
2021-09-17 08:28:46 +0300

    1. Техническое задание - проект "Автонарушители" [#260365]

---
+++ src/main/java/ru/jobj4j/car_accident/App.java
@@ -0,0 +1,7 @@
+package ru.jobj4j.car_accident;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/ru/jobj4j/car_accident/AppTest.java
@@ -0,0 +1,10 @@
+package ru.jobj4j.car_accident;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    private void dump() {
+        assert true;
+    }
+}
