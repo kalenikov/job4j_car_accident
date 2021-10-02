1aec92311ce55bf8e875980a2263b56a038340d6
serj <sergeykalenikov@gmail.com>
2021-10-02 17:06:31 +0300

    2. Регистрация пользователя [#296069 #210681]

--- src/main/java/ru/job4j/accident/control/RegControl.java
+++ src/main/java/ru/job4j/accident/control/RegControl.java
@@ -1,15 +1,19 @@
 package ru.job4j.accident.control;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.User;
 import ru.job4j.accident.repository.data.AuthorityRepository;
 import ru.job4j.accident.repository.data.UserRepository;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RegControl {
@@ -23,12 +27,14 @@ public class RegControl {
         this.authorities = authorities;
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ModelAndView existUsernameException(HttpServletRequest req, DataIntegrityViolationException e) {
+        return new ModelAndView("reg")
+                .addObject("errorMessage", "This username already exists");
+    }
+
     @PostMapping("/reg")
-    public String save(@ModelAttribute User user, Model model) {
-        if (users.findByUsername(user.getUsername()).isPresent()) {
-            model.addAttribute("errorMessage", "This username already exists");
-            return "reg";
-        }
+    public String save(@ModelAttribute User user) {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
